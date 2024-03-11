3ebd5d057546485ac8ae239617689b88df5ce9b0
lane <2751584869@qq.com>
2024-03-11 12:02:17 +0800

    fix

--- src/main/java/edu/hitsz/bim/config/SecurityConfig.java
+++ src/main/java/edu/hitsz/bim/config/SecurityConfig.java
@@ -35,7 +35,7 @@ public class SecurityConfig {
                         .failureHandler(new JsonAuthenticationFailureHandler()))
                 .httpBasic(AbstractHttpConfigurer::disable)
                 .logout(logout -> logout
-                        .logoutUrl("/user/logout")
+                        .logoutUrl("/api/logout")
                         .logoutSuccessHandler(new JsonLogoutSuccessHandler()))
                 .exceptionHandling(h -> h
                         .accessDeniedHandler(new JsonAccessDeniedHandler())
